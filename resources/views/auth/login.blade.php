57dcbb25d5682467aab884dd8acc22f73343fb57
Code Emperor <87604838+LawrenceWoronoff@users.noreply.github.com>
2022-03-19 15:26:44 +0100

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
                                     <div class="mb-3">
                                         <label class="form-label" for="email">Email</label>
                                         <input type="text" class="form-control @error('email') is-invalid @enderror"
-                                            name="email" value="{{ old('email', 'pixabay@gmail.com') }}" id="email"
+                                            name="email" value="{{ old('email') }}" id="email"
                                             placeholder="Enter Email address">
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -54,7 +54,7 @@
                                         </div>
                                         <label class="form-label" for="userpassword">Password</label>
                                         <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                            value="simba98923" name="password" id="userpassword" placeholder="Enter password">
+                                            value="{{old('password')}}" name="password" id="userpassword" placeholder="Enter password">
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
