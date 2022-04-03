1d39b16302cbf84b40412e7929df83ed9a21ba50
Code Emperor <87604838+LawrenceWoronoff@users.noreply.github.com>
2022-04-03 06:50:35 +0200

    Email template fix

--- resources/views/email/reset_email.blade.php
+++ resources/views/email/reset_email.blade.php
@@ -2,8 +2,8 @@
     <div style="background:url({{asset('public/assets/images/email/email_header.jpg')}}) no-repeat;background-size:cover !important; height: 60px;">
     </div>
     <img src="{{asset('public/assets/images/logo-dark.png')}}" style="width:100px; float:right; margin-top:10px;">
-    <h1 style="margin-bottom: 0px; margin-top:50x;">Hi, {{$full_name}}</h1>
-    <h1 style="margin-top: 0px; margin-bottom: 30px;">Welcome to Polymesa</h1>
+    <h2 style="margin-bottom: 0px; margin-top:50x;">Hi, {{$full_name}}</h2>
+    <h2 style="margin-top: 0px; margin-bottom: 30px;">Welcome to Polymesa</h2>
 
     <h3 style="color:black; margin-bottom: 0px;">Reset Password.</h3>
     <p style="color:black; margin-bottom: 0px;">You can click link to reset your password.</p>
